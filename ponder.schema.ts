ea3711e0133c786fa5ad7fecc17ffa108ebfec47
depinindocyber <depin.sutohap@indocyber.id>
2025-07-05 18:52:56 +0700

    feat: define tokensMinted table schema with relevant fields

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,6 +1,12 @@
 import { onchainTable } from "ponder";
 
-export const example = onchainTable("example", (t) => ({
-  id: t.text().primaryKey(),
-  name: t.text(),
+export const tokensMinted = onchainTable("tokensMinted", (t) => ({
+  id: t.text().primaryKey(), // You can use a combination of tx hash + log index for uniqueness
+  to: t.text(),
+  amount: t.bigint(),
+  minter: t.text(),
+  newTotalSupply: t.bigint(),
+  remainingMintable: t.bigint(),
+  reason: t.text(),
+  timestamp: t.bigint(),
 }));
